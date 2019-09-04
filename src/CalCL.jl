f79948f7bc764f6538a0c118eba39350ea936c7d
João Marcello Pereira <jmarcellopereira@ufpi.edu.br>
2019-09-04 18:39:37 -0300

    Update 2

--- src/CalCL.jl
+++ src/CalCL.jl
@@ -1,3 +1,10 @@
+"""
+**PACOTE DIDÁTICO PARA CALCULAR A ÁREA E VOLUME DE CILINDROS.** 
+SINTAXE:
+arealateral(raio, altura)
+areatotal(raio, altura)
+volcilindro(raio, altura)
+"""
 module CalCL
 
 # O código principal do pacote
