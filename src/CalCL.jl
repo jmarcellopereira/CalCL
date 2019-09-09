724cbfdc1059a547a19d498ac31e7006f99b6059
João Marcello Pereira <jmarcellopereira@ufpi.edu.br>
2019-09-09 16:58:09 -0300

    update.
    .

--- src/CalCL.jl
+++ src/CalCL.jl
@@ -13,24 +13,9 @@ module CalCL
 
 # O código principal do pacote
 
-export arealateral
-export volcilindro
-
 include("areaBC.jl")
-
-function arealateral(raio, altura)
-    return  2*pi*raio*altura
-end
-
-function areatotal(raio, altura)
-    println("Área total:")
-    return arealateral(raio, altura) + 2*areaBC(raio)
-end
-
-function volcilindro(raio, altura)
-    area_base = areaBC(raio)
-    println("Volume do Cilindro")
-    return  area_base.*altura
-end
+include("arealateral.jl")
+include("volcilindro.jl")
+include("areatotal.jl")
 
 end
